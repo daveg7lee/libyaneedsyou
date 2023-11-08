50ebbb6a3d80e389e5fad53a4c91e96ce149420f
daveg7lee <daveg7lee@gmail.com>
2023-11-08 19:31:08 +0900

    ✨ 리비아 모금 프로젝트 시작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,193 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import "../styles/style.css";
+import "../styles/reset.css";
+import type { PageProps } from "gatsby";
+import {
+  Box,
+  Button,
+  HStack,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { StaticImage } from "gatsby-plugin-image";
+import { Tweet } from "react-twitter-widgets";
+import Marquee from "react-fast-marquee";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const twitterId = [
+  "1711653049343660319",
+  "1709149943946989699",
+  "1702382183820800253",
+  "1704200342982848643",
+  "1703134192291180576",
+];
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
+    <div className="container">
+      <Box minH="100vh" as="section">
+        <Box position="relative" className="mainImage">
+          <Box
+            position="absolute"
+            left="50%"
+            top="50%"
+            transform="translateX(-50%) translateY(-50%)"
+          >
+            <VStack justifyContent="center" gap="12px">
+              <Text
+                fontSize="65px"
+                color="white"
+                fontWeight="extrabold"
+                textAlign="center"
               >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+                Libya Needs You.
+              </Text>
+              <Text
+                textAlign="center"
+                color="white"
+                fontSize="22px"
+                fontWeight="semibold"
+              >
+                You can help support those in <br /> desperate need with just 1
+                click.
+              </Text>
+              <Button colorScheme="whiteAlpha">Donate</Button>
+            </VStack>
+          </Box>
+        </Box>
+      </Box>
+      <SimpleGrid
+        minH="100vh"
+        p={["20px", "100px"]}
+        columns={[1, 1, 2]}
+        as="section"
+      >
+        <Box>
+          <Text
+            color="black"
+            fontSize="50px"
+            fontWeight="bold"
+            lineHeight="60px"
+          >
+            A country home to the Sahara Desert, Libya
+          </Text>
+          <Text mt="20px" color="grey" fontSize="20px">
+            Located in North Africa, Libya is a country with distinct cultures
+            and is known for its ancient Greek and Roman ruins.
+          </Text>
+        </Box>
+        <StaticImage
+          src="../images/libyamap.webp"
+          alt="Libya Map"
+          objectFit="scale-down"
+        />
+      </SimpleGrid>
+      <Box
+        minH="25vh"
+        bgColor="black"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text
+          textAlign="center"
+          color="white"
+          fontSize={["25", "50px"]}
+          fontWeight="bold"
+        >
+          What happened in Libya?
+        </Text>
+      </Box>
+      <SimpleGrid
+        as="section"
+        minH="100vh"
+        p={["20px", "100px"]}
+        display="grid"
+        columns={[1, 1, 3]}
+        gap="40px"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box borderRadius="7px" overflow="hidden" shadow="base" minH="70vh">
+          <StaticImage
+            src="../images/libyadam.webp"
+            alt="Dam image"
+            height={400}
+            width={480}
+          />
+          <VStack p="30px" alignItems="flex-start">
+            <Text fontWeight="semibold" color="black" fontSize="20px">
+              Storm Daniel bursts 2 dams
+            </Text>
+            <Text fontWeight="medium" color="grey" fontSize="15px">
+              The deadliest Mediterranean tropical cyclone in recorded history
+              shatters the 50-year-old dams.
+            </Text>
+            <Button colorScheme="blackAlpha">Read More</Button>
+          </VStack>
+        </Box>
+        <Box borderRadius="7px" overflow="hidden" shadow="base" minH="70vh">
+          <StaticImage
+            src="../images/flood.webp"
+            alt="Flood image"
+            height={400}
+            width={480}
+          />
+          <VStack p="30px" alignItems="flex-start">
+            <Text fontWeight="semibold" color="black" fontSize="20px">
+              Flood wipes Derna
+            </Text>
+            <Text fontWeight="medium" color="grey" fontSize="15px">
+              A quarter of Derna is gone, and thousands are forced to leave.
+            </Text>
+            <Button colorScheme="blackAlpha">Read More</Button>
+          </VStack>
+        </Box>
+        <Box borderRadius="7px" overflow="hidden" shadow="base" minH="70vh">
+          <StaticImage
+            src="../images/aftermath.webp"
+            alt="Aftermath image"
+            height={400}
+            width={480}
+          />
+          <VStack p="30px" alignItems="flex-start">
+            <Text fontWeight="semibold" color="black" fontSize="20px">
+              Aftermath
+            </Text>
+            <Text fontWeight="medium" color="grey" fontSize="15px">
+              11,000 people died, and 30,000 are missing. Suffering continues.
+            </Text>
+            <Button colorScheme="blackAlpha">Read More</Button>
+          </VStack>
+        </Box>
+      </SimpleGrid>
+      <Box
+        minH="25vh"
+        bgColor="black"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text
+          textAlign="center"
+          color="white"
+          fontSize={["25", "50px"]}
+          fontWeight="bold"
+        >
+          Latest News
+        </Text>
+      </Box>
+      <HStack minH="100vh" py="100px" as="section">
+        <Marquee>
+          {twitterId.map((id) => (
+            <Box mx="6px">
+              <Tweet tweetId={id} />
+            </Box>
+          ))}
+        </Marquee>
+      </HStack>
+    </div>
+  );
+};
 
-export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
+export default IndexPage;
